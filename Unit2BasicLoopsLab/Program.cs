a09b2266185f14b51e250bc8651be7638e0bb965
john <john.poston2@outlook.com>
2022-10-03 21:57:06 -0400

    initial commit

---
+++ Unit2BasicLoopsLab/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Unit2BasicLoopsLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
