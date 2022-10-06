85524e85ba19d71bbe40df8ea9d2def991607316
john <john.poston2@outlook.com>
2022-10-05 21:56:00 -0400

    added limit for incorrect keypad attemps

---
+++ Unit2BasicLoopsLab4/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Unit2BasicLoopsLab4
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int combination;
+            bool unlocked = false;
+            int numAttempts = 0;
+
+            while (unlocked != true)
+            {
+
+                combination = GetCombination();
+                unlocked = VerifyCombination(combination);
+
+                if(unlocked == false)
+                {
+                    numAttempts++;
+
+                    if(numAttempts == 5)
+                    {
+                        Console.WriteLine("You've reached the limit for incorrect attempts.");
+                        Console.WriteLine("Exiting program.");
+                        Environment.Exit(0);
+                    }
+                }
+            }
+
+            Console.WriteLine("You did it! You broke the safe!");
+        }
+
+        static int GetCombination()
+        {
+            int combination = 0;
+
+            Console.Write("Enter the combination. (Hint: it's 13579):");
+            int.TryParse(Console.ReadLine(), out combination);
+
+            return combination;
+        }
+
+        static bool VerifyCombination(int combination)
+        {
+            bool unlocked;
+
+            if (combination == 13579)
+            {
+                unlocked = true;
+            }
+            else
+                unlocked = false;
+
+            return unlocked;
+        }
+
+    }
+}
