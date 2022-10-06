70528c17f13596ad58415a556ff78e19e73d6738
john <john.poston2@outlook.com>
2022-10-05 21:47:05 -0400

    added keypad entry exercise

---
+++ Unit2BasicLoopsLab3/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Unit2BasicLoopsLab3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int combination;
+            bool unlocked = false;
+
+            while (unlocked != true)
+            {
+                
+                combination = GetCombination();
+                unlocked = VerifyCombination(combination);
+            }
+
+            Console.WriteLine("You did it! You broke the safe!");
+            Console.WriteLine("Good thing, too... because you couldn't escape this program if you didn't!");
+        }
+
+        static int GetCombination()
+        {
+            int combination = 0;
+
+            Console.Write("Enter the combination. (Hint: it's 13579):");
+            int.TryParse(Console.ReadLine(), out combination);
+
+            return combination;
+        }
+
+        static bool VerifyCombination(int combination)
+        {
+            bool unlocked;
+
+            if (combination == 13579)
+            {
+                unlocked = true;
+            }
+            else
+                unlocked = false;
+            
+            return unlocked;
+        }
+
+    }
+}
