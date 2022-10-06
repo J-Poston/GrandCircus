36db61c17fc89b9d27172409d1c6d4964e4d9b72
john <john.poston2@outlook.com>
2022-10-05 22:10:16 -0400

    separated main into BreakinAttempt method

---
+++ Unit2BasicLoopsLab5/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Unit2BasicLoopsLab5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int combination;
+            bool unlocked = false;
+            int numAttempts = 0;
+            do
+            {
+
+                combination = GetCombination();
+                unlocked = VerifyCombination(combination);
+
+                if (unlocked == false)
+                {
+                    numAttempts++;
+
+                    if (numAttempts == 5)
+                    {
+                        Console.WriteLine("You've reached the limit for incorrect attempts.");
+                        Console.WriteLine("Exiting program.");
+                        Environment.Exit(0);
+                    }
+                }
+            } while (unlocked != true);
+
+            Console.WriteLine("You did it! You broke the safe!");
+        }
+
+        static int GetCombination()
+        {
+            int combination = 0;
+
+            Console.Write("Enter the combination. (Hint: it's 13579):");
+            int.TryParse(Console.ReadLine(), out combination);
+
+            return combination;
+        }
+
+        static bool VerifyCombination(int combination)
+        {
+            bool unlocked;
+
+            if (combination == 13579)
+            {
+                unlocked = true;
+            }
+            else
+                unlocked = false;
+
+            return unlocked;
+        }
+    }
+}
---
+++ Unit2BasicLoopsLab6/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace Unit2BasicLoopsLab6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool unlocked = BreakinAttempt();
+
+            if(unlocked == true)
+            {
+                Console.WriteLine("You did it! You broke the safe!");
+            }
+
+            else
+            {
+                Console.WriteLine("You've reached the limit for incorrect attempts.");
+            }
+
+            Console.WriteLine("Exiting program.");
+            Environment.Exit(0);
+        }
+
+        static bool BreakinAttempt()
+        {
+            int combination;
+            bool unlocked = false;
+            int numAttempts = 0;
+            do
+            {
+
+                combination = GetCombination();
+                unlocked = VerifyCombination(combination);
+
+                if (unlocked == false)
+                {
+                    numAttempts++;
+
+                    if (numAttempts == 5)
+                    {
+                        break;
+                    }
+                }
+            } while (unlocked != true);
+
+            return unlocked;
+        }
+
+        static int GetCombination()
+        {
+            int combination = 0;
+
+            Console.Write("Enter the combination. (Hint: it's 13579):");
+            int.TryParse(Console.ReadLine(), out combination);
+
+            return combination;
+        }
+
+        static bool VerifyCombination(int combination)
+        {
+            bool unlocked;
+
+            if (combination == 13579)
+            {
+                unlocked = true;
+            }
+            else
+                unlocked = false;
+
+            return unlocked;
+        }
+    }
+}
