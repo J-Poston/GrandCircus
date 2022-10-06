badb2156ed9d502d9a36776d1b7e7e6da32be388
john <john.poston2@outlook.com>
2022-10-05 21:09:00 -0400

    added Hello, World exercise

--- Unit2BasicLoopsLab/Program.cs
+++ Unit2BasicLoopsLab/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string repeat = "y";
+
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine("Hello, World!");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Would you like to continue? (y/n)");
+                repeat = Console.ReadLine();
+
+            } while (repeat.ToLower() == "y");
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Goodbye!");
+            Console.ForegroundColor = ConsoleColor.Gray;
         }
     }
 }
